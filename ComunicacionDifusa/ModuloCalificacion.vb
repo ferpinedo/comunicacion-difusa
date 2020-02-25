76725635ff22d9fcdb105970c212fcb4d38d97ca
Fernando Pinedo <fernando.pinedo@outlook.com>
2020-02-25 09:27:11 -0600

    Remove previous visual code project

--- ComunicacionDifusa/ModuloCalificacion.vb
+++
@@ -1,9 +0,0 @@
-﻿Module ModuloCalificacion
-    Public aciertos
-    Public errores
-    Function Porcentaje(ByVal Exitos, ByVal Intentos) As String
-        Dim calificacion As Double
-        calificacion = Exitos / Intentos
-        Porcentaje = Format(calificacion, "0.0%")
-    End Function
-End Module
--- ComunicacionDifusa/frmIntentaloDeNuevo.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class frmIntentaloDeNuevo
-
-End Class
