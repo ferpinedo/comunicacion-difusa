b58dd29e08974963e007423a1055b99c77d5074b
Fernando Pinedo <fernando.pinedo@outlook.com>
2020-05-04 00:48:35 -0500

    Change justify-center to items-center

--- renderer/pages/index.jsx
+++ renderer/pages/index.jsx
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 const SplashScreen = () => (
   <Layout>
     <div className="flex w-full h-full justify-center items-center">
-      <div className="flex flex-col justify-center space-y-4 mt-20">
+      <div className="flex flex-col items-center space-y-8 mt-20">
         <h1>Comunicación difusa</h1>
         <img src="/images/logo.png" />
         <Link href={`/login`}>
