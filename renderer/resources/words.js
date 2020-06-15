7e32e7f4a8c9360aba81df7f30ab77da37d01223
Fernando Pinedo <fernando.pinedo@outlook.com>
2020-06-14 23:38:56 -0500

    Separate words from ambiguity file

---
+++ renderer/resources/words.js
@@ -0,0 +1,38 @@
+const WORDS = {
+  1: [
+    [
+      {
+        name: "Asia",
+        imagesPaths: ["1.jpg"],
+        meaning: "Asia. Asia es uno de los 5 continentes en el mundo.",
+      },
+      {
+        name: "Hacia",
+        imagesPaths: ["1.jpg", "2.jpg", "3.jpg"],
+        meaning: "Hacia. Me dirijo hacia mi casa.",
+      },
+    ],
+    [
+      {
+        name: "Botar",
+        imagesPaths: ["1.jpg", "2.jpg", "3.jpg"],
+        meaning: "Botar. El niño bota el balón de basquetbol.",
+      },
+      {
+        name: "Votar",
+        imagesPaths: ["1.jpg", "2.jpg", "3.jpg"],
+        meaning:
+          "Votar. Todos los ciudadanos tienen que votar para elegir a un nuevo presidente.",
+      },
+    ],
+  ],
+  2: [["Sierra1", "Sierra2"], ["Bello"], ["Arrollo", "Arroyo"]],
+  3: [
+    ["Aprehender", "Aprender"],
+    ["Ato", "Hato"],
+    ["Balsa", "Valsa"],
+    ["Baya", "Vaya", "Valla"],
+  ],
+};
+
+export default WORDS;
