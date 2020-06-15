b726deab62084082cf95f4f5b48224464063ec02
Elias Snz <eliaas.saanchez@gmail.com>
2020-06-15 14:40:37 -0500

    adding images

--- renderer/resources/words.js
+++ renderer/resources/words.js
@@ -1,19 +1,6 @@
 const WORDS = {
   // Nivel 1
   1: [
-    // Asia y Hacia
-    [
-      {
-        name: "Asia",
-        imagesPaths: ["Asia/1.jpg"],
-        meaning: "Asia. Asia es uno de los 5 continentes en el mundo.",
-      },
-      {
-        name: "Hacia",
-        imagesPaths: ["Hacia/1.jpg", "Hacia/2.jpg", "Hacia/3.jpg"],
-        meaning: "Hacia. Me dirijo hacia mi casa.",
-      },
-    ],
     // Botar y Votar
     [
       {
@@ -28,15 +15,196 @@ const WORDS = {
           "Votar. Todos los ciudadanos tienen que votar para elegir a un nuevo presidente.",
       },
     ],
+    // Sierra, Sierra2 y Cierra
+    [
+      {
+        name: "Sierra",
+        imagesPaths: ["Sierra1/1.jpg", "Sierra1/2.jpg", "Sierra1/3.jpg"],
+        meaning: "Sierra. En la sierra hay muchos árboles.",
+      },
+      {
+        name: "Sierra",
+        imagesPaths: ["Sierra2/1.jpg", "Sierra2/2.jpg", "Sierra2/3.jpg"],
+        meaning: "Sierra. Mi padre necesita una sierra para cortar madera.",
+      },
+      {
+        name: "Cierra",
+        imagesPaths: ["Cierra/1.jpg", "Cierra/2.jpg", "Cierra/3.jpg"],
+        meaning: "Cierra. Cierra la puerta cuando salgas.",
+      },
+    ],
+    // Bello y Vello
+    [
+      {
+        name: "Bello",
+        imagesPaths: ["Bello/1.jpg", "Bello/2.jpg", "Bello/3.jpg"],
+        meaning: "Bello. El paisaje es bello.",
+      },
+      {
+        name: "Vello",
+        imagesPaths: ["Vello/1.jpg", "Vello/2.jpg", "Vello/3.jpg"],
+        meaning: "Vello. Tengo que afeitarme el vello de la cara.",
+      },
+    ],
+    // Coser y Cocer
+    [
+      {
+        name: "Coser",
+        imagesPaths: ["Coser/1.jpg", "Coser/2.jpg", "Coser/3.jpg"],
+        meaning: "Coser. Necesito coser mi pantalón roto.",
+      },
+      {
+        name: "Cocer",
+        imagesPaths: ["Cocer/1.jpg", "Cocer/2.jpg", "Cocer/3.jpg"],
+        meaning: "Coser. Tenemos que cocer la carne.",
+      },
+    ],
   ],
   // Nivel 2
-  2: [["Sierra1", "Sierra2"], ["Bello"], ["Arrollo", "Arroyo"]],
+  2: [
+    // Valla, Baya y Vaya
+    [
+      {
+        name: "Valla",
+        imagesPaths: ["Valla/1.jpg","Valla/2.jpg","Valla/3.jpg"],
+        meaning: "Valla. Mi papá pintará la valla de blanco el fin de semana",
+      },
+      {
+        name: "Baya",
+        imagesPaths: ["Baya/1.jpg", "Baya/2.jpg", "Baya/3.jpg"],
+        meaning: "Baya. El árbol tiene una baya que comerán las ardillas.",
+      },
+      {
+        name: "Vaya",
+        imagesPaths: ["Vaya/1.jpg", "Vaya/2.jpg", "Vaya/3.jpg"],
+        meaning: "Vaya. Mi hermana quiere que vaya esta navidad a su casa.",
+      },
+    ],
+    // Asia y Hacia
+    [
+      {
+        name: "Asia",
+        imagesPaths: ["Valla/1.jpg","Valla/2.jpg","Valla/3.jpg"],
+        meaning: "Asia. Asia es uno de los 5 continentes en el mundo.",
+      },
+      {
+        name: "Hacia",
+        imagesPaths: ["Hacia/1.jpg", "Hacia/2.jpg", "Hacia/3.jpg"],
+        meaning: "Hacia. Me dirijo hacia mi casa.",
+      },
+    ],
+    // Cegar y Segar
+    [
+      {
+        name: "Cegar",
+        imagesPaths: ["Cegar/1.jpg","Cegar/2.jpg","Cegar/3.jpg"],
+        meaning: "Cegar. La luz de la soldadura eléctrica puede cegar.",
+      },
+      {
+        name: "Segar",
+        imagesPaths: ["Segar/1.jpg", "Segar/2.jpg", "Segar/3.jpg"],
+        meaning: "Segar. La máquina va a segar el trigo.",
+      },
+    ],
+    // Arrollo y Arroyo
+    [
+      {
+        name: "Arrollo",
+        imagesPaths: ["Arrollo/1.jpg","Arrollo/2.jpg","Arrollo/3.jpg"],
+        meaning: "Arrollo. Ayer por la noche, casi arrollo a un perro, menos mal las luces del auto estaban encendidas.",
+      },
+      {
+        name: "Arroyo",
+        imagesPaths: ["Arroyo/1.jpg", "Arroyo/2.jpg", "Arroyo/3.jpg"],
+        meaning: "Arroyo. Cerca de la casa en la que crecí, había un arroyo muy bonito, con agua limpia y fresca.",
+      },
+    ],
+    // I.V.A. e Iba
+    [
+      {
+        name: "I.V.A.",
+        imagesPaths: ["IVA/1.jpg","IVA/2.jpg","IVA/3.jpg"],
+        meaning: "IVA. El pantalón me costó 50 dólares con el IVA incluido.",
+      },
+      {
+        name: "Iba",
+        imagesPaths: ["Iba/1.jpg", "Iba/2.jpg", "Iba/3.jpg"],
+        meaning: "Iba. Mi mamá iba para su trabajo en la noche.",
+      },
+    ],
+  ],
   // Nivel 3
   3: [
-    ["Aprehender", "Aprender"],
-    ["Ato", "Hato"],
-    ["Balsa", "Valsa"],
-    ["Baya", "Vaya", "Valla"],
+    // As y As2
+    [
+      {
+        name: "As",
+        imagesPaths: ["As1/1.jpg","As1/2.jpg","As1/3.jpg"],
+        meaning: "As. Soy un as en la escuela.",
+      },
+      {
+        name: "As",
+        imagesPaths: ["As2/1.jpg", "As2/2.jpg", "As2/3.jpg"],
+        meaning: "As. A él le tocó el as y ganó la partida.",
+      },
+    ],
+    // Haz1, Haz2 y Haz3
+    [
+      {
+        name: "Haz",
+        imagesPaths: ["Haz1/1.jpg","Haz1/2.jpg","Haz1/3.jpg"],
+        meaning: "Haz. Haz el quehacer de la casa.",
+      },
+      {
+        name: "Haz",
+        imagesPaths: ["Haz2/1.jpg","Haz2/2.jpg","Haz2/3.jpg"],
+        meaning: "Haz. Como hacía frío metí un haz de leña a la chimenea y la encendí.",
+      },
+      {
+        name: "Haz",
+        imagesPaths: ["Haz3/1.jpg","Haz3/2.jpg","Haz3/3.jpg"],
+        meaning: "Haz. Un haz de luz cae sobre mi ",
+      },
+    ],
+    // Balsa y Valsa
+    [
+      {
+        name: "Balsa",
+        imagesPaths: ["Balsa/1.jpg","Balsa/2.jpg","Balsa/3.jpg"],
+        meaning: "Balsa. El dia de ayer navegamos en una balsa.",
+      },
+      {
+        name: "Valsa",
+        imagesPaths: ["Valsa/1.jpg", "Valsa/2.jpg", "Valsa/3.jpg"],
+        meaning: "Valsa. La novia valsa el vals al ritmo de la música.",
+      },
+    ],
+    // Ato y Hato
+    [
+      {
+        name: "Ato",
+        imagesPaths: ["Ato/1.jpg","Ato/2.jpg","Ato/3.jpg"],
+        meaning: "Ato. Yo ato fuertemente los cordones.",
+      },
+      {
+        name: "Hato",
+        imagesPaths: ["Hato/1.jpg", "Hato/2.jpg", "Hato/3.jpg"],
+        meaning: "Hato. Mis padres salieron ayer con un hato en la maleta.",
+      },
+    ],
+    // Aprender y Apredeher
+    [
+      {
+        name: "Aprender",
+        imagesPaths: ["Aprender/1.jpg","Aprender/2.jpg","Aprender/3.jpg"],
+        meaning: "Aprender. Mañana te toca aprender la tablas de multiplicar.",
+      },
+      {
+        name: "Aprehender",
+        imagesPaths: ["Aprehender/1.jpg", "Aprehender/2.jpg", "Aprehender/3.jpg"],
+        meaning: "Aprehender. Lograron aprehender al ladrón.",
+      },
+    ],
   ],
 };
 
