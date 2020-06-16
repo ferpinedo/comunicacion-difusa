4f982afaad28dd03e81c91e61d68179e73c7b790
Fernando Pinedo <fernando.pinedo@outlook.com>
2020-06-15 23:17:26 -0500

    Clean code

--- renderer/utils/textToSpeech.js
+++ renderer/utils/textToSpeech.js
@@ -1,8 +1,6 @@
 const speak = (text) => {
   const message = new SpeechSynthesisUtterance(text);
   const voices = speechSynthesis.getVoices();
-  console.log("Available voices");
-  console.log(voices);
   message.voice = voices[31];
   message.volume = 1;
   message.pitch = 1;
@@ -11,8 +9,3 @@ const speak = (text) => {
 };
 
 export default speak;
-// VOICES
-// Paulina, es-MX, 31
-// Juan, es-MX, 15
-// Jorge, es-ES, 14
-// Mónica, es-ES, 29
